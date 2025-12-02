a67b367e09f21cc4ed6fbf554060912633e4e9b3
Snehal Mane <snehal6mane@gmail.com>
2025-12-02 23:21:38 +0530

    Wire commands for review radio buttons

--- content_scripts/hide_ratings.js
+++ content_scripts/hide_ratings.js
@@ -13,11 +13,23 @@
     console.log("SHOW RATINGS");
   }
 
+  function showReviews() {
+    console.log("SHOW REVIEWS");
+  }
+
+  function hideReviews() {
+    console.log("HIDE REVIEWS");
+  }
+
   browser.runtime.onMessage.addListener((message) => {
     if (message.command === "HIDE_RATINGS") {
       hideRatings();
     } else if (message.command === "SHOW_RATINGS") {
       showRatings();
+    } else if (message.command === "HIDE_REVIEWS") {
+      hideReviews();
+    } else if (message.command === "SHOW_REVIEWS") {
+      showReviews();
     }
   });
 })();
--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,8 @@
 function listenForSettingChange() {
-  const radios = document.querySelectorAll('input[name="show_ratings"]');
+  const ratingRadios = document.querySelectorAll('input[name="ratings"]');
+  const reviewRadios = document.querySelectorAll('input[name="reviews"]');
 
-  radios.forEach((radio) => {
+  ratingRadios.forEach((radio) => {
     radio.addEventListener("change", (event) => {
       if (event.target.checked) {
         browser.tabs
@@ -14,12 +15,36 @@ function listenForSettingChange() {
     });
   });
 
+  reviewRadios.forEach((radio) => {
+    radio.addEventListener("change", (event) => {
+      if (event.target.checked) {
+        browser.tabs
+          .query({ active: true, currentWindow: true })
+          .then((tabs) => {
+            updateReviewVisibility(tabs, event.target.value === "yes");
+          })
+          .catch(reportScriptError);
+      }
+    });
+  });
+
   function updateRatingVisibility(tabs, showRating) {
     browser.tabs
       .query({ active: true, currentWindow: true })
       .then(() => {
         browser.tabs.sendMessage(tabs[0].id, {
           command: showRating ? "SHOW_RATINGS" : "HIDE_RATINGS",
+        });
+      })
+      .catch(reportScriptError);
+  }
+
+  function updateReviewVisibility(tabs, showReview) {
+    browser.tabs
+      .query({ active: true, currentWindow: true })
+      .then(() => {
+        browser.tabs.sendMessage(tabs[0].id, {
+          command: showReview ? "SHOW_REVIEWS" : "HIDE_REVIEWS",
         });
       })
       .catch(reportScriptError);
